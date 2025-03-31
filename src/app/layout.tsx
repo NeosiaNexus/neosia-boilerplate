fe17db0d3d0ba30d4daacd049b45f102b28e8952
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2025-03-31 18:15:40 +0200

    chore: Configuration linter & prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
 
-import { Poppins } from "next/font/google";
-
-import "./globals.css";
+import './globals.css';
 
 const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 export const metadata: Metadata = {
-  title: "Boilerplate Next.js",
-  description: "Boilerplate Next.js efficae par NeosiaNexus",
+  title: 'Boilerplate Next.js',
+  description: 'Boilerplate Next.js efficae par NeosiaNexus',
 };
 
 export default function RootLayout({
