d261f79009c732d3b490005f37fe711bade7c0bd
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2025-04-02 19:34:11 +0200

    feat: Add user role with better-auth admin plugin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,9 @@
+import { adminClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
+
 const authClient = createAuthClient({
   baseURL: 'http://localhost:3000',
+  plugins: [adminClient()],
 });
 
 export default authClient;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { admin } from 'better-auth/plugins';
 
 import prisma from './prisma';
 
@@ -7,6 +8,7 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  plugins: [admin()],
   emailAndPassword: {
     enabled: true,
     // async sendResetPassword(_data, _request) {
